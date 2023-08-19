13335ae3cc5fd6147030ff3a5ba9ef5f2891c882
Langmao Tw. 浪貓 <98757802+LangmaoLa@users.noreply.github.com>
2023-08-20 02:22:32 +0800

    update /help

--- main.py
+++ main.py
@@ -24,23 +24,31 @@ async def on_member_join(member):
             await member.add_roles(role)
             print(f"已經給予 {member.name} 身分組：{role.name}！")
 
+class HelpMenu(disnake.ui.Select):
+    def __init__(self):
+        options = [
+            disnake.SelectOption(label="role", description="關於身分組的主命令"),
+            disnake.SelectOption(label="role auto", description="設定新成員自動獲得的身分組"),
+            # ... 可以根据需要加入更多命令描述
+        ]
+        super().__init__(placeholder="選擇一個命令來查詢", min_values=1, max_values=1, options=options)
+
+    async def callback(self, inter: disnake.Interaction):
+        selected_command = self.values[0]
+        description = {
+            "role": "關於身分組的主命令...",
+            "role auto": "設定新成員自動獲得的身分組...",
+            # ... 可以根据需要加入更多命令描述
+        }.get(selected_command, "找不到該命令的描述")
+        
+        embed = disnake.Embed(title=f"命令：{selected_command}", description=description, color=0x00FF00)
+        await inter.response.send_message(embed=embed)
+
 @bot.slash_command(description="顯示所有可用的命令和說明")
 async def help(inter):
-    embed = disnake.Embed(title="幫助", description="這是我們的命令列表和說明：", color=0x00FF00)
-
-    embed.add_field(name="/role", value="關於身分組的主命令", inline=False)
-    embed.add_field(name="/role auto", value="設定新成員自動獲得的身分組", inline=False)
-    embed.add_field(name="/role give_everyone", value="給予所有成員指定的身分組", inline=False)
-    embed.add_field(name="/role give_bot", value="給予所有機器人指定的身分組", inline=False)
-    embed.add_field(name="/role give_member", value="給予所有非機器人成員指定的身分組", inline=False)
-    embed.add_field(name="/role give_specify", value="給予特定身分組的成員指定的身分組", inline=False)
-    embed.add_field(name="/role give", value="給予使用者指定的身分組", inline=False)
-    embed.add_field(name="/role remove", value="移除使用者的指定身分組", inline=False)
-    embed.add_field(name="/role create", value="在伺服器中創建一個新的身分組", inline=False)
-    embed.add_field(name="/role delete", value="刪除指定的身分組", inline=False)
-
-    await inter.response.send_message(embed=embed)
-
+    view = disnake.ui.View()
+    view.add_item(HelpMenu())
+    await inter.response.send_message("選擇一個命令來查詢:", view=view)
 
 bot.load_extension("cmds.role_commands")  # 載入 role_commands 模組
 
