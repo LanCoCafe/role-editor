71bb9f3db7f297f3fe1cbc3685b8c5b4823b6890
Langmao Tw. 浪貓 <98757802+LangmaoLa@users.noreply.github.com>
2023-08-24 16:21:02 +0800

    Help

--- main.py
+++ main.py
@@ -27,8 +27,19 @@ async def on_member_join(member):
 class HelpMenu(disnake.ui.Select):
     def __init__(self):
         options = [
-            disnake.SelectOption(label="/role give ", description="關於身分組的主命令"),
-            disnake.SelectOption(label="/role auto", description="設定新成員自動獲得的身分組"),
+            disnake.SelectOption(label="/role auto", description="設定新使用者的指定身分組"),
+            disnake.SelectOption(label="/role give_everyone", description="給予所有人指定身分組"),
+            disnake.SelectOption(label="/role give_bot", description="設定新成員自動獲得的身分組"),
+            disnake.SelectOption(label="/role give_member", description="設定新成員自動獲得的身分組"),
+            disnake.SelectOption(label="/role give_specify", description="設定新成員自動獲得的身分組"),
+            disnake.SelectOption(label="/role give", description="移除一位使用者指定身分組"),
+            disnake.SelectOption(label="/role remove", description="移除一位使用者指定身分組"),
+            disnake.SelectOption(label="/role create", description="創建身分組"),
+            disnake.SelectOption(label="/role delete", description="刪除身分組"),
+            # disnake.SelectOption(label="", description="設定新成員自動獲得的身分組"),
+            # disnake.SelectOption(label="/role auto", description="設定新成員自動獲得的身分組"),
+            # disnake.SelectOption(label="/role auto", description="設定新成員自動獲得的身分組"),
+            # disnake.SelectOption(label="/role auto", description="設定新成員自動獲得的身分組"),
             # ... 可以根据需要加入更多命令描述
         ]
         super().__init__(placeholder="選擇一個命令來查詢", min_values=1, max_values=1, options=options)
@@ -36,8 +47,15 @@ class HelpMenu(disnake.ui.Select):
     async def callback(self, inter: disnake.Interaction):
         selected_command = self.values[0]
         description = {
-            "/role give": "關於身分組的主命令...",
-            "/role auto": "設定新成員自動獲得的身分組...",
+            "/role auto": "設定新使用者的指定身分組",
+            "/role give_everyone": "給予所有人指定身分組",
+            "/role give_bot": "給予所有機器人指定身分組",
+            "/role give_member": "給予所有使用者指定身分組",
+            "/role give_specify": "給予擁有此身分組(target_role)的使用者(role)指定身分組",
+            "/role give": "給予一位使用者指定身分組",
+            "/role remove": "移除一位使用者指定身分組",
+            "/role create <身分組名稱>": "創建身分組",
+            "/role delete": "刪除身分組",
             # ... 可以根据需要加入更多命令描述
         }.get(selected_command, "找不到該命令的描述")
         
@@ -46,7 +64,7 @@ class HelpMenu(disnake.ui.Select):
 
 class LinkButton(disnake.ui.Button):
     def __init__(self):
-        super().__init__(style=disnake.ButtonStyle.link, label="LanCo Café", url="https://discord.gg/seZ3WNTC4J")
+        super().__init__(style=disnake.ButtonStyle.link, label="LanCo Café 官方支援伺服器", url="https://discord.gg/seZ3WNTC4J")
 
 @bot.slash_command(description="顯示所有可用的命令和說明")
 async def help(inter):
@@ -60,5 +78,3 @@ bot.load_extension("cmds.role_commands")  # 載入 role_commands 模組
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 bot.run(TOKEN)
-
-# Hello World!
